4f7e39deaecbae07d7df209f74b41b4ce8b2a447
Chris Dzombak <chris@dzombak.com>
2024-05-07 15:31:59 -0400

    modernize the script; add --verbose and --debug-log-ratelimit

--- auditor.py
+++ auditor.py
@@ -2,18 +2,21 @@
 
 import argparse
 import datetime
+import dns
 import json
 import os
 import requests
 import sys
-from dns import name, resolver, reversename
+
+from dns import resolver
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def eprint(*argss, **kwargs):
+    print(*argss, file=sys.stderr, **kwargs)
 
 
 class AuthException(Exception):
@@ -34,7 +37,8 @@ class APIException(Exception):
 
     @property
     def human_str(self):
-        return 'API Error: {msg:s}\n{method:s}: {url:s}\nHTTP Status: {status}\nError Detail:\n{detail}'.format(
+        return ('API Error: {msg:s}\n{method:s}: {url:s}\nHTTP Status: {status}\nError Detail:\n{'
+                'detail}').format(
             msg=self.__str__(),
             status=self.status_code or '[unknown]',
             detail=json.dumps(self.errors, sort_keys=True, indent=2),
@@ -61,10 +65,11 @@ class HTTPBearerAuth(requests.auth.AuthBase):
 
 
 class DigitalOceanAPI(object):
+    _API_BASE = 'https://api.digitalocean.com/v2'
+    logRatelimit: bool
 
-    API_BASE = 'https://api.digitalocean.com/v2'
-
-    def __init__(self, token):
+    def __init__(self, token: str):
+        self.logRatelimit = False
         self.auth = HTTPBearerAuth(token)
 
     def _check_response(self, r):
@@ -81,9 +86,9 @@ class DigitalOceanAPI(object):
             )
 
     def _get(self, endpoint, params=None):
-        url = '{base:s}/{endpoint:s}'.format(base=DigitalOceanAPI.API_BASE, endpoint=endpoint)
+        url = '{base:s}/{endpoint:s}'.format(base=DigitalOceanAPI._API_BASE, endpoint=endpoint)
         resp = requests.get(url, auth=self.auth, params=params)
-        # self._log_ratelimit(resp)
+        self._log_ratelimit(resp)
         self._check_response(resp)
         return resp
 
@@ -91,14 +96,17 @@ class DigitalOceanAPI(object):
         return self._get(endpoint, params).json()
 
     def _log_ratelimit(self, response):
+        if not self.logRatelimit:
+            return
+
         ratelimit = response.headers.get('RateLimit-Limit')
         remaining = response.headers.get('RateLimit-Remaining')
         reset = response.headers.get('RateLimit-Reset')
         if not ratelimit or not remaining or not reset:
             return
-        reset_dt = datetime.datetime.utcfromtimestamp(int(reset.strip()))\
+        reset_dt = datetime.datetime.utcfromtimestamp(int(reset.strip())) \
             .replace(tzinfo=datetime.timezone.utc)
-        eprint(" -  DO Rate Limit: {:s}/{:s} remain; reset {:s}".format(
+        eprint(" [DO Rate Limit] {:s}/{:s} remain; reset {:s}".format(
             remaining, ratelimit, reset_dt.isoformat(' ')))
 
     def check_auth(self):
@@ -112,11 +120,16 @@ class DigitalOceanAPI(object):
 
 
 class Auditor(object):
+    verbose: bool
 
-    def __init__(self, do_api):
-        self.do_api = do_api
+    def __init__(self, do_api_instance):
+        self.do_api = do_api_instance
+        self.verbose = False
         self.resolver = resolver.Resolver(configure=False)
-        self.resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+        self.resolver.nameservers = [
+            '8.8.8.8', '1.1.1.1',
+            '2001:4860:4860::8888', '2606:4700:4700::1111',
+        ]
 
     def audit_all(self):
         """
@@ -128,7 +141,7 @@ class Auditor(object):
             retv = retv & self.audit(n)
         return retv
 
-    def audit(self, domain_name):
+    def audit(self, domain_name: str):
         """
         Returns False if any anomalies were found; True otherwise.
         """
@@ -139,31 +152,65 @@ class Auditor(object):
             if r['type'] not in ('A', 'AAAA'):
                 continue
             try:
-                ptr_addr = reversename.from_address(r['data'])
-                rev_answer = self.resolver.query(ptr_addr, "PTR")
-            except (resolver.NoAnswer, resolver.NXDOMAIN):
+                ptr_addr = dns.reversename.from_address(r['data'])
+                rev_answer = self.resolver.resolve(ptr_addr, "PTR", lifetime=10.0)
+            except (resolver.NoAnswer, resolver.NXDOMAIN, resolver.NoNameservers):
                 print("[i] {recname:s}: No rDNS found for {ip:s}".format(
                     recname=r['name'], ip=r['data']))
                 continue
+            except resolver.LifetimeTimeout:
+                print("[i] {recname:s}: rDNS lookup for {ip:s} timed out"
+                      .format(recname=r['name'], ip=r['data']))
+                retv = False
+                continue
+            except dns.exception.DNSException as e2:
+                print("[!] {recname:s}: rDNS lookup for {ip:s} failed: {exc:s}"
+                      .format(recname=r['name'], ip=r['data'], exc=str(e2)))
+                retv = False
+                continue
             for a in rev_answer:
+                if self.verbose:
+                    print(f"    {r['name']}: {r['type']} -> {r['data']} -> {a}")
                 try:
-                    fwd_name = name.from_text(str(a))
-                    fwd_answer = self.resolver.query(fwd_name, r['type'])
-                except (resolver.NoAnswer, resolver.NXDOMAIN):
+                    fwd_name = dns.name.from_text(str(a))
+                    fwd_answer = self.resolver.resolve(fwd_name, r['type'], lifetime=10.0)
+                except (resolver.NoAnswer, resolver.NXDOMAIN, resolver.NoNameservers):
                     print("[!] {recname:s}: Reverse DNS for {ip:s} is {revname:s}, but no forward "
                           "{type:s} record for {revname:s} exists."
                           .format(recname=r['name'], ip=r['data'], revname=str(a), type=r['type']))
                     retv = False
                     continue
+                except resolver.LifetimeTimeout:
+                    print("[!] {recname:s}: Reverse DNS for {ip:s} is {revname:s}, but forward "
+                          "{type:s} lookup for {revname:s} timed out"
+                          .format(recname=r['name'], ip=r['data'], revname=str(a), type=r['type']))
+                    retv = False
+                    continue
+                except dns.exception.DNSException as e2:
+                    print(
+                        "[!] {recname:s}: Reverse DNS for {ip:s} is {revname:s}, but forward "
+                        "{type:s} lookup for {revname:s} failed: {exc:s}".format(
+                            recname=r['name'],
+                            ip=r['data'],
+                            revname=str(a),
+                            type=r['type'],
+                            exc=str(e2),
+                        ))
+                    retv = False
+                    continue
                 for fwd_ip in fwd_answer:
+                    if self.verbose:
+                        print(f"    {r['name']}: {r['type']} -> {r['data']} -> {a} -> {fwd_ip}")
                     if str(fwd_ip) != r['data']:
-                        print("[!] {recname:s}: Reverse DNS for {ip:s} is {revname:s} which has: "
-                              "{type:s} -> {fwd_ip:s}.".format(
+                        print(
+                            "[!] {recname:s}: Reverse DNS for {ip:s} is {revname:s} which has: "
+                            "{type:s} -> {fwd_ip:s}.".format(
                                 recname=r['name'],
                                 ip=r['data'],
                                 revname=str(a),
                                 type=r['type'],
-                                fwd_ip=str(fwd_ip)))
+                                fwd_ip=str(fwd_ip),
+                            ))
                         retv = False
                         continue
         return retv
@@ -176,13 +223,21 @@ if __name__ == '__main__':
                "environment variable. It can also be provided via a .env file, in this directory "
                "(see .env.sample).")
     parser.add_argument('--domain', type=str,
-                        help="The domain to audit. If left empty, all domains in the account will be audited.")
+                        help="The domain to audit. "
+                             "If left empty, all domains in the account will be audited.")
     parser.add_argument('--fail-return', type=int, default=3,
                         help="Return code in case audit problems were detected. Default: 3.")
+    parser.add_argument('--debug-log-ratelimit', action='store_true',
+                        help="Log DigitalOcean API rate limit information to stderr.")
+    parser.add_argument('--verbose', action='store_true',
+                        help="Print each check as it is performed, regardless of outcome.")
     args = parser.parse_args()
 
     if args.fail_return in (1, 2):
-        eprint("Error: Return codes 1 and 2 are reserved for general and authentication errors, respectively.")
+        eprint(
+            "Error: Return codes 1 and 2 are reserved for general "
+            "and authentication errors, respectively."
+        )
         sys.exit(1)
 
     do_token = os.getenv('DIGITALOCEAN_TOKEN')
@@ -191,6 +246,7 @@ if __name__ == '__main__':
         eprint("Copy .env.sample to .env and fill it out to provide credentials.")
         sys.exit(2)
     do_api = DigitalOceanAPI(do_token)
+    do_api.logRatelimit = args.debug_log_ratelimit
     try:
         do_api.check_auth()
     except AuthException:
@@ -206,6 +262,7 @@ if __name__ == '__main__':
     if args.domain:
         domain_name = args.domain.lower().strip()
     auditor = Auditor(do_api)
+    auditor.verbose = args.verbose
     try:
         if domain_name:
             result = auditor.audit(domain_name)
